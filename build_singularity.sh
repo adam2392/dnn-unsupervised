00003c72414d4d40b2bfcfb597a2254288fef3c4
Adam Li <adam2392@gmail.com>
2018-01-27 10:50:35 +0100

    Adding separate shell script file for getting the singularity images in order.

---
+++ build_singularity.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ml singularity 
+
+echo "Make sure you cd'ed to the scratch directory!"
+
+cd /scratch/users/$USER/dnn-unsupervised/
+
+# pull tensorflow image onto the marcc-hpc
+singularity pull --name tensorflow.simg shub://marcc-hpc/tensorflow docker://python:latest
+
