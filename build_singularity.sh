a62cae468a715faecfd9cb85af99e5015acf2335
Adam Li <adam2392@gmail.com>
2018-01-29 22:07:32 +0100

    Adding python dnn training file.

--- build_singularity.sh
+++ build_singularity.sh
@@ -7,5 +7,9 @@ echo "Make sure you cd'ed to the scratch directory!"
 cd /scratch/users/$USER/dnn-unsupervised/
 
 # pull tensorflow image onto the marcc-hpc
-singularity pull --name tensorflow.simg shub://marcc-hpc/tensorflow 
+# singularity pull --name tensorflow.simg shub://marcc-hpc/tensorflow 
 
+# singularity pull --name tensorflow.simg shub://belledon/tensorflow-keras:speech
+
+rm tensorflow.simg
+singularity pull --name tensorflow.simg shub://marcc-hpc/python3-keras-tensorflow
