da9f799bff4972daa1bfaca135f893d3fb25a6a6
Adam Li <adam2392@gmail.com>
2018-02-07 23:58:11 +0100

    lowering learning rate.

--- main.py
+++ main.py
@@ -169,14 +169,14 @@ if __name__ == '__main__':
 
     # This will do preprocessing and realtime data augmentation:
     datagen = keras.preprocessing.image.ImageDataGenerator(
-                    featurewise_center=True,  # set input mean to 0 over the dataset
+                    featurewise_center=False,  # set input mean to 0 over the dataset
                     samplewise_center=False,  # set each sample mean to 0
-                    featurewise_std_normalization=True,  # divide inputs by std of the dataset
+                    featurewise_std_normalization=False,  # divide inputs by std of the dataset
                     samplewise_std_normalization=False,  # divide each input by its std
                     zca_whitening=False,      # apply ZCA whitening
                     rotation_range=0,         # randomly rotate images in the range (degrees, 0 to 180)
-                    width_shift_range=0.1,    # randomly shift images horizontally (fraction of total width)
-                    height_shift_range=0.1,   # randomly shift images vertically (fraction of total height)
+                    width_shift_range=0.0,    # randomly shift images horizontally (fraction of total width)
+                    height_shift_range=0.0,   # randomly shift images vertically (fraction of total height)
                     horizontal_flip=False,    # randomly flip images
                     vertical_flip=False,      # randomly flip images
                     fill_mode='nearest')  
