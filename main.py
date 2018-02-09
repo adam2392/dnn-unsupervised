b7e37dd13dec56e8c81169d08af23b745b23c424
Adam Li <adam2392@gmail.com>
2018-02-09 17:33:40 +0100

    Running a dnn training with auc as well.

--- main.py
+++ main.py
@@ -178,10 +178,10 @@ if __name__ == '__main__':
 
     # This will do preprocessing and realtime data augmentation:
     datagen = keras.preprocessing.image.ImageDataGenerator(
-                    featurewise_center=True,  # set input mean to 0 over the dataset
-                    samplewise_center=False,  # set each sample mean to 0
-                    featurewise_std_normalization=True,  # divide inputs by std of the dataset
-                    samplewise_std_normalization=False,  # divide each input by its std
+                    featurewise_center=False,  # set input mean to 0 over the dataset
+                    samplewise_center=True,  # set each sample mean to 0
+                    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+                    samplewise_std_normalization=True,  # divide each input by its std
                     zca_whitening=False,      # apply ZCA whitening
                     rotation_range=0,         # randomly rotate images in the range (degrees, 0 to 180)
                     width_shift_range=0.0,    # randomly shift images horizontally (fraction of total width)
