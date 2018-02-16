e3354ca2076773729566191e77b4c08be4979a32
Adam Li <adam2392@gmail.com>
2018-02-16 18:33:01 +0100

    Adding v3 of training on realdata.

--- bin/trainbasecnn/main.py
+++ bin/trainbasecnn/main.py
@@ -158,14 +158,14 @@ if __name__ == '__main__':
 
     # This will do preprocessing and realtime data augmentation:
     datagen = keras.preprocessing.image.ImageDataGenerator(
-                    # featurewise_center=True,  # set input mean to 0 over the dataset
-                    samplewise_center=True,  # set each sample mean to 0
-                    # featurewise_std_normalization=True,  # divide inputs by std of the dataset
-                    samplewise_std_normalization=True,  # divide each input by its std
+                    featurewise_center=True,  # set input mean to 0 over the dataset
+                    # samplewise_center=True,  # set each sample mean to 0
+                    featurewise_std_normalization=True,  # divide inputs by std of the dataset
+                    # samplewise_std_normalization=True,  # divide each input by its std
                     zca_whitening=False,      # apply ZCA whitening
-                    # rotation_range=3,         # randomly rotate images in the range (degrees, 0 to 180)
-                    # width_shift_range=0.02,    # randomly shift images horizontally (fraction of total width)
-                    # height_shift_range=0.02,   # randomly shift images vertically (fraction of total height)
+                    rotation_range=3,         # randomly rotate images in the range (degrees, 0 to 180)
+                    width_shift_range=0.02,    # randomly shift images horizontally (fraction of total width)
+                    height_shift_range=0.02,   # randomly shift images vertically (fraction of total height)
                     horizontal_flip=False,    # randomly flip images
                     vertical_flip=False,      # randomly flip images
                     # channel_shift_range=4,
