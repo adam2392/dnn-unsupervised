ac02659e24d27335aab613ee330a2da4028ab215
Adam Li <adam2392@gmail.com>
2018-03-30 21:12:11 -0700

    Adding the main training cnn module. for 1dcnn.

--- dnn/model/train/traincnn.py
+++ dnn/model/train/traincnn.py
@@ -271,9 +271,6 @@ class TrainCNN(BaseTrain):
         # Xmain_train = np.concatenate(Xmain_train, axis=0)
         Xmain_train = np.vstack(Xmain_train)
 
-        self.X_train = Xmain_train
-        self.y_train = y_train
-
         print(y_train.shape)
         print(Xmain_train.shape)
         # load the ylabeled data 1 in 0th position is 0, 1 in 1st position is 1
@@ -284,12 +281,16 @@ class TrainCNN(BaseTrain):
                                                           np.unique(
                                                               y_train).astype(int),
                                                           np.argmax(y_train, axis=1))
+        self.X_train = Xmain_train
+        self.y_train = y_train
         self.class_weight = class_weight
 
     def loadtestingdata_vars(self, Xmain_test, y_test):
-        y_test = np.array(y_test)
-        Xmain_test = np.concatenate(Xmain_test, axis=0)
-
+        y_test = np.array(y_test)[:,np.newaxis]
+        Xmain_test = np.vstack(Xmain_test)
+        # load the ylabeled data 1 in 0th position is 0, 1 in 1st position is 1
+        invert_y = 1 - y_test
+        y_test = np.concatenate((invert_y, y_test, axis=1))
         self.X_test = Xmain_test
         self.y_test = y_test
 
