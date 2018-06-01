0b48843a325fa0c1824ae3a82e241e40500c0c7c
Adam Li <adam2392@gmail.com>
2018-06-01 20:16:53 +0200

    Adding test script.

--- bin_main/pytorch/mnist/main.py
+++ bin_main/pytorch/mnist/main.py
@@ -17,7 +17,7 @@ import dnn_pytorch
 import dnn_pytorch.base.constants.model_constants as constants
 from dnn_pytorch.io.read_dataset import Reader
 from dnn_pytorch.base.dataset.fftdataset import FFT2DImageDataset
-# from dnn_pytorch.models.trainers.trainer import Trainer
+from dnn_pytorch.models.trainers.trainer import Trainer
 from dnn_pytorch.models.nets.cnn import ConvNet
 
 # # preprocessing data
