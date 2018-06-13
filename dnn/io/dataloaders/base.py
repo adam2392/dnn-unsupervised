10eba16f3436b0f8c0adebc6abc73b2c655051f0
Adam Li <adam2392@gmail.com>
2018-06-13 11:44:57 +0200

    Pushing up working model of keras version.

---
+++ dnn/io/dataloaders/base.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import json
+import os
+
+from dnn.base.constants.config import Config
+from dnn.base.utils.log_error import initialize_logger
+import dnn.base.constants.model_constants as constants
+from sklearn.utils import compute_class_weight
+
+# preprocessing data
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
+
+class TrainDataset(object):
+    X_train = None
+    y_train = None
+    class_weight = None
+
+    def __len__(self):
+        return len(self.X_train)
+
+    @property
+    def imsize(self):
+        return self.X_train.shape[2]
+
+    @property
+    def n_colors(self):
+        return self.X_train.shape[3]
+
+class TestDataset(object):
+    X_test = None
+    y_test = None
+    class_weight = None
+
+    def __len__(self):
+        return len(self.X_test)
+
+    @property
+    def imsize(self):
+        return self.X_train.shape[2]
+
+    @property
+    def n_colors(self):
+        return self.X_train.shape[3]
+
+class ReaderImgDataset(object):
+    root_dir = None
+    patients = None
+    testfilepaths = None
+    trainfilepaths = None
+    filelist = None
+
+    train_dataset = TrainDataset()
+    test_dataset = TestDataset()
+
+    def __init__(self, config=None):
+        self.config = config or Config()
+        self.logger = initialize_logger(
+            self.__class__.__name__,
+            self.config.out.FOLDER_LOGS)
+
+    def __len__(self):
+        return len(self.filelist)
+
+    def loadbydir(self, traindir, testdir, procedure='loo', testname=None):
+        raise NotImplementedError("Need to implement function to load filepaths\
+                for all datasets into list.")
+
+    def loadfiles(self, filelist=[], mode=constants.TRAIN):
+        raise NotImplementedError("Need to implement function to load files into memory.")
+
+    def _formatdata(self, images):
+        images = images.swapaxes(1, 3)
+        # lower sample by casting to 32 bits
+        images = images.astype("float32")
+        return images
+
+    @property
+    def imsize(self):
+        return self.train_dataset.X_train.shape[2]
+
+    @property
+    def n_colors(self):
+        return self.train_dataset.X_train.shape[3]
+
+    def getchanstats(self, chanaxis=3):
+        '''
+        Chan axis = 3 if using keras/tensorflow
+        Chan axis = 1 if using pytorch
+        '''
+        numchans = self.train_dataset.X_train.shape[chanaxis]
+
+        chanmeans = []
+        chanstd = []
+        for ichan in range(numchans):
+            chandata = self.train_dataset.X_train[...,ichan].ravel()
+            chanmeans.append(np.mean(chandata))
+            chanstd.append(np.std(chandata))
+        self.chanmeans = np.array(chanmeans)
+        self.chanstd = np.array(chanstd)
