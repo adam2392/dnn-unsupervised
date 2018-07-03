b40afc9dd666bf3329604f458cee9d460acbd2f6
Adam Li <adam2392@gmail.com>
2018-07-03 14:59:44 +0200

    Fixing bug for predicting classes.

--- dnn/keras_models/trainers/eznet.py
+++ dnn/keras_models/trainers/eznet.py
@@ -148,8 +148,8 @@ class EZNetTrainer(BaseTrainer):
             y_pred_max = K.max(y_pred, axis=1)
             y_pred_max = K.reshape(y_pred_max, (K.shape(y_pred)[0], 1))
             y_pred_max_mat = K.equal(y_pred, y_pred_max)
-            for c_p, c_t in product(range(nb_cl), range(nb_cl)):
-                final_mask += (weights[c_t, c_p] * y_pred_max_mat[:, c_p] * y_true[:, c_t])
+            
+            final_mask += (weights * y_pred_max_mat[:, :] * y_true[:, :])
             return K.categorical_crossentropy(y_pred, y_true) * final_mask
         from keras.losses import binary_crossentropy
         def weighted_binary_crossentropy(y_true, y_pred):
